55f7f641d3bec9e0a3c4c5e4af4344dfa4a969a5
Max Taylor <maxbransontaylor@gmail.com>
2022-06-18 23:26:03 -0400

    refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,28 @@
 var formE1 = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-var createTaskHandler = function (event) {
+var taskFormHandler = function (event) {
   event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput,
+  };
+  createTaskE1(taskDataObj);
+};
+var createTaskE1 = function (taskDataObj) {
   var listItemE1 = document.createElement("li");
   listItemE1.className = "task-item";
-  listItemE1.textContent = "new task";
+  var taskInfoE1 = document.createElement("div");
+  taskInfoE1.className = "task-item";
+  taskInfoE1.innerHTML =
+    "<h3 class='task-name'>" +
+    taskDataObj.name +
+    "</h3>" +
+    "<span class= 'task-type'>" +
+    taskDataObj.type +
+    "</span>";
+  listItemE1.appendChild(taskInfoE1);
   tasksToDoEl.appendChild(listItemE1);
-  console.log(event);
 };
-formE1.addEventListener("submit", createTaskHandler);
+formE1.addEventListener("submit", taskFormHandler);
