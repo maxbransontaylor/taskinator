1e7a6913b260b6cf961bb7a876adf4da98cc5ab1
Max Taylor <maxbransontaylor@gmail.com>
2022-06-18 22:42:21 -0400

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
-var buttonE1 = document.querySelector("#save-task");
+var formE1 = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+  event.preventDefault();
   var listItemE1 = document.createElement("li");
   listItemE1.className = "task-item";
   listItemE1.textContent = "new task";
   tasksToDoEl.appendChild(listItemE1);
+  console.log(event);
 };
-buttonE1.addEventListener("click", createTaskHandler);
+formE1.addEventListener("submit", createTaskHandler);
