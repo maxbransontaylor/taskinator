2708aec3655f8d3d7473d408306da0c15e74adcd
Max Taylor <maxbransontaylor@gmail.com>
2022-06-23 16:55:03 -0400

    what

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var buttonE1 = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var taskIdcounter = 0;
 var createTaskHandler = function () {
   var listItemE1 = document.createElement("li");
   listItemE1.className = "task-item";
